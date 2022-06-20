6fd2e8eaf09fa35d9a7246868773739645b75fcd
Marcia Alves <marciasoalves@gmail.com>
2022-06-20 20:50:05 -0300

    Movimentos possíveis de uma peça

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -44,6 +44,10 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)){
             throw new ChessException("não tem peça nessa posição do tabuleiro");
         }
+        if(!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("não existe movimento para a peça escolhida");
+
+        }
     }
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
